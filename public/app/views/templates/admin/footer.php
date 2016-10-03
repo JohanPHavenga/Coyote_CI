f7935aa63de1190441d353cf6051eff03cb93586
Johan Havenga <johan.havenga@gmail.com>
2016-10-03 22:16:53 +0200

    Theme change complete
    Theme change to layout 2 complete

--- public/app/views/templates/admin/footer.php
+++ public/app/views/templates/admin/footer.php
@@ -1,35 +1,31 @@
-                                </div>
-                                <!-- END PAGE CONTENT INNER -->                                
                             </div>
                         </div>
-                        <!-- END PAGE CONTENT BODY -->
-                        <!-- END CONTENT BODY -->
-                    </div>
-                    <!-- END CONTENT -->
-                </div>
-                <!-- END CONTAINER -->
-            </div>
-        </div>
-        <div class="page-wrapper-row">
-            <div class="page-wrapper-bottom">
-                <!-- BEGIN FOOTER -->
-                <!-- BEGIN INNER FOOTER -->
-                <div class="page-footer">
-                    <div class="container-fluid"> 2016 &copy; Project Codename
-                        <a target="_blank" href="http://www.johan.havenga.co.za">Coyote</a>
                     </div>
                 </div>
-                <div class="scroll-to-top">
-                    <i class="icon-arrow-up"></i>
-                </div>
-                <!-- END INNER FOOTER -->
-                <!-- END FOOTER -->
+                <!-- END PORTRET -->
+
+
+            </div>
+            <!-- END CONTENT BODY -->
+        </div>
+        <!-- END CONTENT -->
+    </div>
+    <!-- END CONTAINER -->
+
+    <!-- BEGIN FOOTER -->
+    <div class="page-footer">
+        <div class="page-footer-inner">
+             2016 &copy; Project Codename
+            <a target="_blank" href="http://www.johan.havenga.co.za">Coyote</a>
+            <div class="scroll-to-top">
+                <i class="icon-arrow-up"></i>
             </div>
         </div>
+
         <!--[if lt IE 9]>
         <script src="<?= base_url('plugins/respond.min.js');?>"></script>
-        <script src="<?= base_url('plugins/excanvas.min.js');?>"></script> 
-        <script src="<?= base_url('plugins/ie8.fix.min.js');?>"></script> 
+        <script src="<?= base_url('plugins/excanvas.min.js');?>"></script>
+        <script src="<?= base_url('plugins/ie8.fix.min.js');?>"></script>
         <![endif]-->
         <!-- BEGIN CORE PLUGINS -->
         <script src="<?= base_url('plugins/jquery.min.js');?>" type="text/javascript"></script>
@@ -41,9 +37,9 @@
         <!-- END CORE PLUGINS -->
         <?php
         // load extra JS files from controller
-            if (isset($js_to_load)) : 
+            if (isset($js_to_load)) :
                 foreach ($js_to_load as $row):
-                    $js_link=base_url($row);        
+                    $js_link=base_url($row);
                     echo "<script src='$js_link' type='text/javascript'></script>";
                 endforeach;
             endif;
@@ -53,9 +49,9 @@
         <!-- END THEME GLOBAL SCRIPTS -->
         <?php
         // load script files from controller
-            if (isset($scripts_to_load)) : 
+            if (isset($scripts_to_load)) :
                 foreach ($scripts_to_load as $row):
-                    $js_link=base_url($row);        
+                    $js_link=base_url($row);
                     echo "<script src='$js_link' type='text/javascript'></script>";
                 endforeach;
             endif;
