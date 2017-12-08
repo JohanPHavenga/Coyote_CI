382ed84fc2c0e1f93307596b5c554b2a8e4d04de
Johan Havenga <johan.havenga@gmail.com>
2017-12-08 09:34:50 +0200

    Old function removed

--- public/app/controllers/Event.php
+++ public/app/controllers/Event.php
@@ -181,69 +181,6 @@ class Event extends Frontend_Controller {
     }
     
     
-    public function detail_old($edition_name_encoded) {
-        // as daar nie 'n edition_name deurgestuur word nie
-        if ($edition_name_encoded=="index") { redirect("/event/calendar");  }
-
-        $edition_name=urldecode($edition_name_encoded);
-
-        $this->load->model('edition_model');
-        $this->load->model('race_model');
-
-        $edition_id=$this->edition_model->get_edition_id_from_name($edition_name);
-
-        if (!$edition_id)
-        {
-            // if name cannot be matched to an edition
-            $this->session->set_flashdata([
-                'alert'=>"Error trying to load the event details. Please try selecting it again from the list below.",
-                'status'=>"danger",
-                ]);
-            redirect("/event/calendar");
-        }
-        else
-        {
-            $this->data_to_header['title']=$edition_name;
-        }
-
-        // set data to view
-        $this->data_to_header['css_to_load']=array();
-        $this->data_to_footer['js_to_load']=array();
-        $this->data_to_footer['scripts_to_load']=array();
-
-        // get event details
-        $this->data_to_view['event_detail']=$this->edition_model->get_edition_detail_full($edition_id);
-        $this->data_to_view['event_detail']['race_list']=$this->race_model->get_race_list(100,0,$edition_id);
-
-        // check if events is in the past
-        $this->data_to_view['notice']='';
-        if ($this->data_to_view['event_detail']['edition_date'] < date("Y-m-d")) {
-            $this->data_to_view['notice']="<div class='alert alert-warning' role='alert'>Please note that you are viewing an event that has happend in the past.</div>";
-        }
-
-        $crumbs=[
-            "Details"=>"",
-            "Events"=>"/event/calendar",
-            "Home"=>"/",
-        ];
-
-        // set title bar
-        $this->data_to_view["title_bar"]=$this->render_topbar_html(
-            [
-                "title"=>$this->data_to_view['event_detail']['event_name'],
-                "sub_title"=>date("Y",strtotime($this->data_to_view['event_detail']['edition_date']))." Edition",
-                "crumbs"=>$crumbs,
-            ]);
-
-
-        // load view
-        $this->load->view($this->header_url, $this->data_to_header);
-        $this->load->view("/event/detail", $this->data_to_view);
-        $this->load->view($this->footer_url, $this->data_to_footer);
-
-    }
-    
-
     function ics($edition_id) {
 
         $this->load->model('edition_model');
